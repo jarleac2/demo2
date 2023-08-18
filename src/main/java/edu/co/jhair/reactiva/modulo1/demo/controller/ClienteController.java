31b26827cc4f1764be15368115c3d19f1cbb0614
Jhair Leal <jarleac@hotmail.com>
2023-08-18 16:50:41 -0500

    ajustes aws kafka

--- src/main/java/edu/co/jhair/reactiva/modulo1/demo/controller/ClienteController.java
+++ src/main/java/edu/co/jhair/reactiva/modulo1/demo/controller/ClienteController.java
@@ -1,10 +1,16 @@
 package edu.co.jhair.reactiva.modulo1.demo.controller;
 
+import ch.qos.logback.core.net.server.Client;
+import edu.co.jhair.reactiva.modulo1.demo.dto.DTOClienteNombreActivo;
 import edu.co.jhair.reactiva.modulo1.demo.model.Cliente;
+import edu.co.jhair.reactiva.modulo1.demo.service.ClienteKafkaConsumerService;
+import edu.co.jhair.reactiva.modulo1.demo.service.ClienteSQSService;
 import edu.co.jhair.reactiva.modulo1.demo.service.ClienteService;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/cliente")
@@ -12,8 +18,15 @@ public class ClienteController {
 
     ClienteService clienteService;
 
-    public ClienteController(ClienteService clienteService){
+    ClienteKafkaConsumerService clienteKafkaConsumerService;
+    ClienteSQSService clienteSQSService;
+
+    public ClienteController(ClienteService clienteService,
+                             ClienteKafkaConsumerService clienteKafkaConsumerService,
+                             ClienteSQSService clienteSQSService) {
         this.clienteService = clienteService;
+        this.clienteKafkaConsumerService = clienteKafkaConsumerService;
+        this.clienteSQSService = clienteSQSService;
     }
 
     @GetMapping("/{id}")
@@ -52,6 +65,37 @@ public class ClienteController {
     @DeleteMapping("/")
     public Mono<Void> deleteAllClientes(){
         return clienteService.deleteAll();
+    }
+
+    @PostMapping("/cliente-por-activo-por-descripcion")
+    public Flux<Cliente> getClientesByActivoByDescripcion(@RequestBody DTOClienteNombreActivo dtoClienteNombreActivo){
+        return clienteService.findByNombreActivo(dtoClienteNombreActivo.nombre(),dtoClienteNombreActivo.activo());
+    }
+
+    @GetMapping("/topico-kakfa/{topico}")
+    public Mono<String> getClienteFromTopicoKafka(@PathVariable String topico) {
+        return Mono.just(clienteKafkaConsumerService.obtenerUltimoCliente(topico));
+    }
+
+    @PostMapping("/aws/createQueue")
+    public Mono<String> postCreateQueue(@RequestBody Map<String, Object> requestBody){
+        return Mono.just(clienteSQSService.createStandardQueue((String) requestBody.get("queueName")));
+    }
+
+    @PostMapping("/aws/postMessageQueue/{queueName}")
+    public Mono<String> postMessageQueue(@RequestBody Cliente cliente, @PathVariable String queueName){
+        return Mono.just(clienteSQSService.publishStandardQueueMessage(
+                queueName,
+                2,
+                cliente));
+    }
+
+    @PostMapping("/aws/processClienteByNombre")
+    public Mono<Cliente> deleteClienteFromQueueByNombre(@RequestBody Map<String, Object> requestBody){
+        return clienteSQSService.deleteClienteMessageInQueue((String) requestBody.get("queueName"),
+                (Integer) requestBody.get("maxNumberMessages"),
+                (Integer) requestBody.get("waitTimeSeconds"),
+                (String) requestBody.get("nombreCliente"));
     }
 
 }
